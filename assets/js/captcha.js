5d6556681ec3a55a07fd42639f1942af41eccb3d
ledgermail <ledgermail.io@gmail.com>
2019-02-15 12:03:07 +0530

    update

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,56 +1,55 @@
 (function() {
-  
   var onSubmit = function(response) {
-    return new Promise(function(resolve, reject){
-        if(response){
-            $("#contact-form").ajaxSubmit({
-              target: qf_results,
-              dataType: "json",
-              success: function(data) {
-                var type =
-                  data.result === "error" ? "alert-danger" : "alert-success";
-                qf_results
-                  .removeClass("alert-danger alert-success")
-                  .addClass("alert " + type)
-                  .html(data.message)
-                  .slideDown(400);
-                if (data.result !== "error") {
-                  $(form)
-                    .clearForm()
-                    .find(".input-field")
-                    .removeClass("input-focused");
-                }
-              }
-            });
-        }
-        resolve();
-    })
+    return new Promise(function(resolve, reject) {
+      if (response) {
+        $("#contact-form").ajaxSubmit({
+          target: qf_results,
+          dataType: "json",
+          success: function(data) {
+            var type =
+              data.result === "error" ? "alert-danger" : "alert-success";
+            qf_results
+              .removeClass("alert-danger alert-success")
+              .addClass("alert " + type)
+              .html("Thank you!")
+              .slideDown(400);
+            if (data.result !== "error") {
+              $("#contact-form")
+                .clearForm()
+                .find(".input-field")
+                .removeClass("input-focused");
+            }
+          }
+        });
+      }
+      resolve();
+    });
   };
   var onSubmitPop = function(response) {
-    return new Promise(function(resolve, reject){
-        if(response){
-            $("#p-contact-form").ajaxSubmit({
-              target: qf_results,
-              dataType: "json",
-              success: function(data) {
-                var type =
-                  data.result === "error" ? "alert-danger" : "alert-success";
-                qf_results
-                  .removeClass("alert-danger alert-success")
-                  .addClass("alert " + type)
-                  .html(data.message)
-                  .slideDown(400);
-                if (data.result !== "error") {
-                  $(form)
-                    .clearForm()
-                    .find(".input-field")
-                    .removeClass("input-focused");
-                }
-              }
-            });
-        }
-        resolve();
-    })
+    return new Promise(function(resolve, reject) {
+      if (response) {
+        $("#p-contact-form").ajaxSubmit({
+          target: qf_results_p,
+          dataType: "json",
+          success: function(data) {
+            var type =
+              data.result === "error" ? "alert-danger" : "alert-success";
+              qf_results_p
+              .removeClass("alert-danger alert-success")
+              .addClass("alert " + type)
+              .html("Thank you!")
+              .slideDown(400);
+            if (data.result !== "error") {
+              $("#p-contact-form")
+                .clearForm()
+                .find(".input-field")
+                .removeClass("input-focused");
+            }
+          }
+        });
+      }
+      resolve();
+    });
   };
 
   onLoad = function() {
@@ -59,9 +58,9 @@
       callback: onSubmitPop
     });
     contactFormWidget = grecaptcha.render("contact-form-captcha", {
-        sitekey: "6Lf5X5EUAAAAAOsnMMtfxZKXioIOuK1gGLf2vF8E",
-        callback: onSubmit
-      });
+      sitekey: "6Lf5X5EUAAAAAOsnMMtfxZKXioIOuK1gGLf2vF8E",
+      callback: onSubmit
+    });
   };
 
   if (!$().validate || !$().ajaxSubmit) {
@@ -87,16 +86,16 @@
     });
   }
 
-  var contactForm = $("#p-contact-form");
-  if (contactForm.length > 0) {
-    var selectRec = contactForm.find("select.required"),
-      qf_results = contactForm.find(".form-results");
-    contactForm.validate({
+  var pContactForm = $("#p-contact-form");
+  if (pContactForm.length > 0) {
+    var selectRec = pContactForm.find("select.required"),
+      qf_results_p = pContactForm.find(".form-results");
+      pContactForm.validate({
       invalidHandler: function() {
-        qf_results.slideUp(400);
+        qf_results_p.slideUp(400);
       },
       submitHandler: function(form) {
-        qf_results.slideUp(400);
+        qf_results_p.slideUp(400);
         grecaptcha.execute(contactPopWidget);
       }
     });
