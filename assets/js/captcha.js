f1a9bf313f4d58225067348c2fc56b65a1142d62
naga2003 <72598989+naga2003@users.noreply.github.com>
2021-06-07 09:25:35 +0530

    Update captcha.js

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,6 +1,5 @@
 (function () {
   var onSubmit = function (response) {
-    console.log(response);
     return new Promise(function (resolve, reject) {
       if (response) {
         $("#contact-form").ajaxSubmit({
