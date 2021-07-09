58b6b64c44bfb096bca4d3e1ea85d87d453ecc88
nagnath602 <nagnath602@gmail.com>
2021-07-09 18:33:25 +0530

    changes

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,19 +1,34 @@
 function sendEmail() {
-  //
-  fetch(
-    "https://7q4us37nyd.execute-api.us-east-2.amazonaws.com/default/preregister-form",
-    {
-      method: "POST",
-      body: {
-        name: "naganath",
-        email: "naganath@pingalasoftware.com",
-      }, // The data
-      headers: {
-        "Content-type": "application/json",
-        // The type of data you're sending
-      },
-    }
-  ).then(function (response) {
-    alert(response);
+  var preregister = $("#preregister-form");
+  preregister.validate({
+    invalidHandler: function () {
+      console.log("1");
+    },
+    submitHandler: function (form) {
+      console.log("🚀 ~ file: sendMail.js ~ line 8 ~ sendEmail ~ form", form);
+      console.log("2");
+    },
   });
+  var alertmessage = preregister.find(".message");
+  var name = document.getElementById("preregister-name");
+  email = preregister.find(".email");
+  age = preregister.find(".age");
+
+  if (name.length && email.length && age.length) {
+    alertmessage
+      .removeClass("alert alert-danger alert-success")
+      .addClass("alert alert-success")
+      .html("Thank you!");
+  }
+  setTimeout(() => {
+    if (contactForm.length > 0) {
+      alert(
+        "pre registration successful. Please check your email for confirmation"
+      );
+    }
+  }, 2000);
+
+  setTimeout(() => {
+    preregister.clearForm();
+  }, 10000);
 }
