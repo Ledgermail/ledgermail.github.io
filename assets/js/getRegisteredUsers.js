9155205f4e6968bffe2e4049529537f511f27a4c
nagnath602 <nagnath602@gmail.com>
2021-07-28 13:12:05 +0530

    updates

--- assets/js/getRegisteredUsers.js
+++ assets/js/getRegisteredUsers.js
@@ -1,8 +1,9 @@
 document.getElementById("download").addEventListener("click", DownloadJsonData);
+
 document.getElementById("submitPassword").addEventListener("click", verifyUser);
 
 var xmlHttp = new XMLHttpRequest();
-let users = {};
+
 function verifyUser() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
@@ -17,52 +18,34 @@ function verifyUser() {
     auth.classList.toggle("display");
     auth.classList.toggle("notDisplay");
     xmlHttp.open(
-      "GET",
-      "https://pl-reg.herokuapp.com/ledgermail-pre-registrations",
-      false
-    );
-    xmlHttp.send(null);
-    users = JSON.parse(xmlHttp.responseText);
-    document.getElementById("count").innerHTML = users.total;
-    let row = document.getElementById("dataTable");
+  "GET",
+  "https://pl-reg.herokuapp.com/ledgermail-pre-registrations",
+  false
+);
+xmlHttp.send(null);
+users = JSON.parse(xmlHttp.responseText);
+document.getElementById("count").innerHTML = users.total;
+let row = document.getElementById("dataTable");
 
-    for (let i = 0; i < users.total; i++) {
-      let tr = document.createElement("tr");
+for (let i = 0; i < users.total; i++) {
+  let tr = document.createElement("tr");
 
-      tr.innerHTML = `
+  tr.innerHTML = `
     <td> ${i + 1}</td>
     <td> ${users.data[i].name}</td>
     <td> ${users.data[i].email}</td>
     <td> ${users.data[i].age}</td>
     <td> ${formatDate(new Date(users.data[i].time))}</td>
    `;
-      row.appendChild(tr);
-    }
-    row.innerHTML;
+  row.appendChild(tr);
+}
+row.innerHTML;
   } else {
     document.getElementById("message").innerText =
       " wrong username or password";
   }
 }
 
-function verifyUser() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if (username === "secretUser@pingala" && password === "Pingala@software.#") {
-    var datable = document.getElementById("datatables");
-    var datasection = document.getElementById("datasection");
-    var auth = document.getElementById("auth");
-    datable.classList.toggle("notDisplay");
-    datable.classList.toggle("display");
-    datasection.classList.toggle("notDisplay");
-    datasection.classList.toggle("display");
-    auth.classList.toggle("display");
-    auth.classList.toggle("notDisplay");
-  } else {
-    document.getElementById("message").innerText =
-      " wrong username or password";
-  }
-}
 
 function formatDate(date) {
   var hours = date.getHours();
