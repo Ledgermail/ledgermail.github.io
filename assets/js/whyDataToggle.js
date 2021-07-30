33fa85200033992964e2cad0822dc252cd38fe50
nagnath602 <nagnath602@gmail.com>
2021-07-30 11:51:10 +0530

    added why features

--- assets/js/whyDataToggle.js
+++ assets/js/whyDataToggle.js
@@ -9,13 +9,26 @@ function updateSecurity(feature) {
   );
   var id = ["security", "privacy", "encryption", "centralized"];
   for (var i = 0; i < id.length; i++) {
-      if (id[i] === feature) {
-         document.getElementById(feature+"Icon").classList.toggle("rotate");
+    if (id[i] === feature) {
+      if (
+        document.getElementById(feature + "Icon").classList.contains("rotate")
+      ) {
+        document.getElementById(feature + "Icon").classList.add("revert");
+        document.getElementById(feature + "Icon").classList.remove("rotate");
+      } else {
+        document.getElementById(feature + "Icon").classList.add("rotate");
+        document.getElementById(feature + "Icon").classList.remove("revert");
+      }
+
       continue;
     } else {
-          document.getElementById(id[i]).classList.remove("show");
-         document.getElementById(id[i] + "Icon").classList.remove("rotate");
-          
+      document.getElementById(id[i]).classList.remove("show");
+      if (
+        document.getElementById(id[i] + "Icon").classList.contains("rotate")
+      ) {
+        document.getElementById(id[i] + "Icon").classList.remove("rotate");
+        document.getElementById(id[i] + "Icon").classList.add("revert");
+      }
     }
   }
 }
