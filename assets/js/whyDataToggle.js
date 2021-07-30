7f566563eba14d405098d6beafb103a213fc3045
nagnath602 <nagnath602@gmail.com>
2021-07-31 01:08:20 +0530

    updated latest contents

--- assets/js/whyDataToggle.js
+++ assets/js/whyDataToggle.js
@@ -62,3 +62,30 @@ function updateWhy(feature) {
     }
   }
 }
+
+function Interoperability(feature) {
+  var id = ["signUp", "interface", "isolation", "receive"];
+  for (var i = 0; i < id.length; i++) {
+    if (id[i] === feature) {
+      if (
+        document.getElementById(feature + "Icon").classList.contains("rotate")
+      ) {
+        document.getElementById(feature + "Icon").classList.add("revert");
+        document.getElementById(feature + "Icon").classList.remove("rotate");
+      } else {
+        document.getElementById(feature + "Icon").classList.add("rotate");
+        document.getElementById(feature + "Icon").classList.remove("revert");
+      }
+
+      continue;
+    } else {
+      document.getElementById(id[i]).classList.remove("show");
+      if (
+        document.getElementById(id[i] + "Icon").classList.contains("rotate")
+      ) {
+        document.getElementById(id[i] + "Icon").classList.remove("rotate");
+        document.getElementById(id[i] + "Icon").classList.add("revert");
+      }
+    }
+  }
+}
