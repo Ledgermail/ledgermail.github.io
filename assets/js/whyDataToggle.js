a1c2f228ef6d4bb1bf565f516335b85225ed3d94
nagnath602 <nagnath602@gmail.com>
2021-07-30 22:17:57 +0530

    updated problems and why section

--- assets/js/whyDataToggle.js
+++ assets/js/whyDataToggle.js
@@ -2,12 +2,42 @@
 //   .getElementById("securityHeader")
 //   .addEventListener("click", updateSecurity);
 
-function updateSecurity(feature) {
-  console.log(
-    "🚀 ~ file: whyDataToggle.js ~ line 7 ~ updateSecurity ~ feature",
-    feature
-  );
+function updateProblem(feature) {
+ 
   var id = ["security", "privacy", "encryption", "centralized"];
+  for (var i = 0; i < id.length; i++) {
+    if (id[i] === feature) {
+      if (
+        document.getElementById(feature + "Icon").classList.contains("rotate")
+      ) {
+        document.getElementById(feature + "Icon").classList.add("revert");
+        document.getElementById(feature + "Icon").classList.remove("rotate");
+      } else {
+        document.getElementById(feature + "Icon").classList.add("rotate");
+        document.getElementById(feature + "Icon").classList.remove("revert");
+      }
+
+      continue;
+    } else {
+      document.getElementById(id[i]).classList.remove("show");
+      if (
+        document.getElementById(id[i] + "Icon").classList.contains("rotate")
+      ) {
+        document.getElementById(id[i] + "Icon").classList.remove("rotate");
+        document.getElementById(id[i] + "Icon").classList.add("revert");
+      }
+    }
+  }
+}
+
+
+function updateWhy(feature) {
+  var id = [
+    "whyReplacement",
+    "whySecurity",
+    "whyEncryption",
+    "whyDecentralization",
+  ];
   for (var i = 0; i < id.length; i++) {
     if (id[i] === feature) {
       if (
