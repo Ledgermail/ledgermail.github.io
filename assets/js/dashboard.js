92ee15e467f0a92a4c988a3a33734a84ec479e79
nagnath602 <nagnath602@gmail.com>
2021-08-07 15:58:23 +0530

    updates

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,6 +1,10 @@
 var xmlHttp = new XMLHttpRequest();
+document.getElementById("refresh").addEventListener("click", getData);
+
 async function getData() {
-  xmlHttp.open("GET", "http://pl-reg.herokuapp.com/user-count", true);
+  document.getElementById("refresh").disabled = true;
+  document.getElementById("refresh").style.color = "grey";
+  xmlHttp.open("GET", "http://pl-reg.herokuapp.com/user-count", false);
   xmlHttp.send(null);
 
   //   data = await fetch("http://pl-reg.herokuapp.com/user-count", {
@@ -12,8 +16,12 @@ async function getData() {
   //     "🚀 ~ file: dashboard.js ~ line 6 ~ getData ~ xmlHttp.responseText",
   //     data
   //   );
-  count = await JSON.parse(xmlHttp.responseText);
-  console.log("🚀 ~ file: dashboard.js ~ line 10 ~ getData ~ count", count);
+  // count = await JSON.parse(xmlHttp.responseText);
+  let x = 0;
+  x = await JSON.parse(xmlHttp.responseText);
+  document.getElementById("totalSignups").innerHTML = x.totalRegistrations;
+   await document.getElementById("refresh").disabled = false;
+   await document.getElementById("refresh").style.color = "white";
 }
 
 getData();
