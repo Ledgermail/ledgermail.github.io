f606c6cc7e3a89ba72cd2daace0390b06c0afaef
nagnath602 <nagnath602@gmail.com>
2021-08-07 15:45:25 +0530

    updates

--- assets/js/dashboard8.js
+++ assets/js/dashboard8.js
@@ -1,4 +1,6 @@
 var xmlHttp = new XMLHttpRequest();
+document.getElementById("refresh").addEventListener("click", getData);
+
 async function getData() {
   xmlHttp.open("GET", "http://pl-reg.herokuapp.com/user-count", false);
   xmlHttp.send(null);
@@ -15,7 +17,7 @@ async function getData() {
   // count = await JSON.parse(xmlHttp.responseText);
   let x = 0;
   console.log("test", (x = await JSON.parse(xmlHttp.responseText)));
-  document.getElementById("topic").innerHTML = x.totalRegistrations;
+  document.getElementById("totalSignups").innerHTML = x.totalRegistrations;
 }
 
 getData();
