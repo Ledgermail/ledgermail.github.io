20fa378b099774c58d5eb93fd689a4891fbc5bea
nagnath602 <nagnath602@gmail.com>
2021-08-07 15:26:39 +0530

    added media pulbications

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,24 +1,24 @@
-var x = setInterval(function () {
-  var now = new Date().getTime();
-  var deadline = new Date("Aug 04, 2021 18:30:00").getTime();
-  var t = deadline - now;
+// var x = setInterval(function () {
+//   var now = new Date().getTime();
+//   var deadline = new Date("Aug 04, 2021 18:30:00").getTime();
+//   var t = deadline - now;
 
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  var hours = Math.floor(t / (1000 * 60 * 60));
-  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((t % (1000 * 60)) / 1000);
+//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor(t / (1000 * 60 * 60));
+//   var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((t % (1000 * 60)) / 1000);
 
- 
-   document.getElementById("timer2").innerHTML =
-     ("0" + hours).slice(-2) +
-     ":" +
-     ("0" + minutes).slice(-2) +
-     ":" +
-     ("0" + seconds).slice(-2);
-  if (t < 0) {
-    clearInterval(x);
-    document.getElementById("timer2").innerHTML = "";
+//  /*  */
+//    document.getElementById("timer2").innerHTML =
+//      ("0" + hours).slice(-2) +
+//      ":" +
+//      ("0" + minutes).slice(-2) +
+//      ":" +
+//      ("0" + seconds).slice(-2);
+//   if (t < 0) {
+//     clearInterval(x);
+//     document.getElementById("timer2").innerHTML = "";
 
-    // document.getElementById("topic").innerHTML = "Coming Soon! Stay Tuned!";
-  }
-}, 1000);
+//     // document.getElementById("topic").innerHTML = "Coming Soon! Stay Tuned!";
+//   }
+// }, 1000);
